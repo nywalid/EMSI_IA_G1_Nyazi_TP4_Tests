0efb6e1b166f7dc611e663a33d776d3713904232
walid <walidnyazi17@gmail.com>
2025-11-14 19:01:30 +0100

    Test 2 - LoggingNYAZI

---
+++ src/main/java/ma/emsi/nyazi/Test2/Assistant.java
@@ -0,0 +1,5 @@
+package ma.emsi.nyazi.Test2;
+
+public interface Assistant {
+    String chat(String message);
+}
---
+++ src/main/java/ma/emsi/nyazi/Test2/RagNaif2.java
@@ -0,0 +1,104 @@
+package ma.emsi.nyazi.Test2;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+
+public class RagNaif2{
+    private static void configureLogger() {
+        // Configure le logger sous-jacent (java.util.logging)
+        Logger packageLogger = Logger.getLogger("dev.langchain4j");
+        packageLogger.setLevel(Level.FINE); // Ajuster niveau
+        // Ajouter un handler pour la console pour faire afficher les logs
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINE);
+        packageLogger.addHandler(handler);
+    }
+
+    public static void main(String[] args) throws  Exception {
+        String cle= System.getenv("GEMINI_KEY");
+        ChatModel modele = GoogleAiGeminiChatModel
+                .builder()
+                .apiKey(cle)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.7)
+                .logRequestsAndResponses(true)
+                .build();
+
+        Path pdf = Paths.get("src/main/resources/rag.pdf");
+        DocumentParser documentParser = new ApacheTikaDocumentParser();
+        Document document = FileSystemDocumentLoader.loadDocument(pdf,documentParser);
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(500, 50);
+
+        List<TextSegment> segments = splitter.split(document);
+
+
+        EmbeddingModel embeddingModel= new AllMiniLmL6V2EmbeddingModel();
+        Response<List<Embedding>> response= embeddingModel.embedAll(segments);
+        List<Embedding> listeEmbedding= response.content();
+
+        EmbeddingStore<TextSegment> embeddingStore= new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(listeEmbedding, segments);
+
+
+        ContentRetriever contentRetriever=
+                EmbeddingStoreContentRetriever.builder()
+                        .embeddingStore(embeddingStore)
+                        .embeddingModel(embeddingModel)
+                        .maxResults(2)
+                        .minScore(0.5)
+                        .build();
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatModel(modele)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .contentRetriever(contentRetriever)
+                .build();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
+    }
+}
