5db4e7e963d43e6c4da4d3b315376ef1ca920fc3
walid <walidnyazi17@gmail.com>
2025-11-14 18:44:01 +0100

    Phase 1 du test 1 NYAZI

--- src/main/java/ma/emsi/nyazi/Test1/RagNaif.java
+++ src/main/java/ma/emsi/nyazi/Test1/RagNaif.java
@@ -1,7 +1,24 @@
 package ma.emsi.nyazi.Test1;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class RagNaif {
     public static void main(String[] args) {
@@ -12,5 +29,21 @@ public class RagNaif {
                 .modelName("gemini-2.5-flash")
                 .temperature(0.7)
                 .build();
+
+        Path pdf = Paths.get("src/main/resources/rag.pdf");
+        DocumentParser documentParser = new ApacheTikaDocumentParser();
+        Document document = FileSystemDocumentLoader.loadDocument(pdf,documentParser);
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(500, 50);
+
+        List<TextSegment> segments = splitter.split(document);
+
+
+        EmbeddingModel embeddingModel= new AllMiniLmL6V2EmbeddingModel();
+        Response<List<Embedding>> response= embeddingModel.embedAll(segments);
+        List<Embedding> listeEmbedding= response.content();
+
+        EmbeddingStore<TextSegment> embeddingStore= new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(listeEmbedding, segments);
     }
 }
