d80cbf43616b283e89ce09899b0667c1b43c4dc0
walid <walidnyazi17@gmail.com>
2025-11-14 18:50:53 +0100

    Phase 2 du test 1 NYAZI

--- src/main/java/ma/emsi/nyazi/Test1/RagNaif.java
+++ src/main/java/ma/emsi/nyazi/Test1/RagNaif.java
@@ -1,5 +1,6 @@
 package ma.emsi.nyazi.Test1;
 
+import com.sun.jna.Memory;
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.DocumentParser;
 import dev.langchain4j.data.document.DocumentSplitter;
@@ -8,17 +9,22 @@ import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import dev.langchain4j.model.output.Response;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class RagNaif {
     public static void main(String[] args) {
@@ -45,5 +51,38 @@ public class RagNaif {
 
         EmbeddingStore<TextSegment> embeddingStore= new InMemoryEmbeddingStore<>();
         embeddingStore.addAll(listeEmbedding, segments);
+
+
+        ContentRetriever contentRetriever=
+                EmbeddingStoreContentRetriever.builder()
+                        .embeddingStore(embeddingStore)
+                        .embeddingModel(embeddingModel)
+                        .maxResults(2)
+                        .minScore(0.5)
+                        .build();
+
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatModel(modele)
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .contentRetriever(contentRetriever)
+                .build();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
     }
 }
